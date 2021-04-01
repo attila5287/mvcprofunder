b489a0a078bc48c5c80f9222654e019461c0f9c5
attila5287 <atiturkoz@hotmail.com>
2021-04-01 16:12:27 -0600

    landing json test

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,16 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const { Project, User } = require('../models');
 
 router.use('/api', apiRoutes);
+
+router.get('/', async (req, res) => {
+  try {
+    const pros = await Project.findAll({ include: { all: true } });
+    res.status(200).json(pros);
+  } catch (error) {
+    res.status(400).json(err);
+  }
+});
 
 module.exports = router;
