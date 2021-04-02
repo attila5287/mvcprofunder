02819696f22aa6cf3b2e3811bed1675f7ed4d9ae
attila5287 <atiturkoz@hotmail.com>
2021-04-02 11:06:42 -0600

    theme journal

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,4 +2,8 @@ module.exports = {
   calculate_progress: (need, coll) => {
     return Math.floor((coll / need) * 100);
   },
+  format_amount: (amount) => {
+    // format large numbers with commas
+    return parseInt(amount).toLocaleString();
+  },
 };
