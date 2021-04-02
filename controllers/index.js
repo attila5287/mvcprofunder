225f9f954d24102aff048e91520b54be663bad61
attila5287 <atiturkoz@hotmail.com>
2021-04-02 08:28:39 -0600

    heroku home

--- controllers/index.js
+++ controllers/index.js
@@ -1,21 +1,31 @@
 const path = require('path');
 const router = require('express').Router();
 const apiRoutes = require('./api');
-
 const { Project, User } = require('../models');
+
 router.use('/api', apiRoutes);
 
-router.get('/', (req, res) => {
-  try {
-    res.sendFile(path.join(__dirname, '../views/layouts/main.html'));
-  } catch (error) {
-    res.status(500).json(err);
-  }
+// route to get all dishes
+router.get('/', async (req, res) => {
+  // We find all dishes in the db and set the data equal to dishData
+  const proData = await Project.findAll({ include: { all: true } }).catch(
+    (err) => {
+      res.json(err);
+    }
+  );
+  // We use map() to iterate over dishData and then add .get({ plain: true }) each object to serialize it.
+  const all = proData.map((p) => p.get({ plain: true }));
+  // We render the template, 'all', passing in dishes, a new array of serialized objects.
+  // res.status(200).json(all);
+  res.render('all', { all });
 });
+
 router.get('/r', async (req, res) => {
   try {
     const pros = await Project.findAll({ include: { all: true } });
-    res.status(200).json(pros);
+    // res.status( 200 ).json( pros );
+    const all = pros.map((p) => p.get({ plain: true }));
+    res.status(200).json(all);
   } catch (error) {
     res.status(400).json(err);
   }
