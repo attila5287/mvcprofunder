1e5e04d101e9a99c3116e13d66cf8b23bdf10c17
attila5287 <atiturkoz@hotmail.com>
2021-04-02 16:02:45 -0600

    login logout works but not smooth

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -45,7 +45,7 @@ router.post('/login', async (req, res) => {
       
       res.json({ user: userData, message: 'You are now logged in!' });
     });
-
+    
   } catch (err) {
     res.status(400).json(err);
   }
@@ -54,7 +54,8 @@ router.post('/login', async (req, res) => {
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
-      res.status(204).end();
+      res.json({ message: 'You are now logged out!' });
+      // res.status(204).end();
     });
   } else {
     res.status(404).end();
