df4056a86a5d9303573c54d4b85f27cd10eab883
attila5287 <atiturkoz@hotmail.com>
2021-04-03 01:37:20 -0600

    user profile route

--- controllers/index.js
+++ controllers/index.js
@@ -18,7 +18,7 @@ router.get('/', async (req, res) => {
   const all = proData.map((p) => p.get({ plain: true }));
   // We render the template, 'all', passing in dishes, a new array of serialized objects.
   // res.status(200).json(all);
-  res.render('all', { all, logged_in: req.session.logged_in, });
+  res.render('all', { all, logged_in: req.session.logged_in,  user_id: req.session.user_id, });
 });
 
 
@@ -30,15 +30,50 @@ router.get( '/login', ( req, res ) => {
     res.redirect('/');
     return;
   }
-  h
   // Otherwise, render the 'login' template
   res.render('login');
 });
 
-router.get('/new_project', (req, res) => {
-  res.render('new_project');
+router.get('/new_project', withAuth, (req, res) => {
+  res.render('new_project',{ logged_in: req.session.logged_in});
 });
 
+
+// route to get all dishes
+router.get('/profile/:id',withAuth, async (req, res) => {
+  // We find all dishes in the db and set the data equal to dishData
+  const user_model = await User.findByPk(req.params.id, { include: { all: true } }).catch(
+    (err) => {
+      res.json(err);
+    }
+  );
+  // We use map() to iterate over dishData and then add .get({ plain: true }) each object to serialize it.
+  const user = user_model.get( { plain: true } );
+  console.log(user);
+
+  // We render the template, 'all', passing in dishes, a new array of serialized objects.
+  // res.status(200).json(all);
+  res.render('profile', {user,  logged_in: req.session.logged_in});
+});
+
+// GET a single reader
+router.get('/:id', async (req, res) => {
+  try {
+    const readerData = await Reader.findByPk(req.params.id, {
+      // Add Book as a second model to JOIN with
+      include: [{ model: LibraryCard }, { model: Book }],
+    });
+
+    if (!readerData) {
+      res.status(404).json({ message: 'No reader found with that id!' });
+      return;
+    }
+
+    res.status(200).json(readerData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
 router.get('/pros', async (req, res) => {
