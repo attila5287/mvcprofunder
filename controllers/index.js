332aa287fb8fd6e5af1a06eee0b13e46edb0a8df
attila5287 <atiturkoz@hotmail.com>
2021-04-03 07:20:47 -0600

    titles /projects/:id href

--- controllers/index.js
+++ controllers/index.js
@@ -38,6 +38,14 @@ router.get('/new_project', withAuth, (req, res) => {
   res.render('new_project',{ logged_in: req.session.logged_in, user_id: req.session.user_id,});
 });
 
+router.get('/projects/:id', withAuth, async (req, res) => {
+  const pro = await Project.findByPk( req.params.id, { include: { all: true } } ).catch(e=>console.log(e))
+  // res.json( pro.get( { plain: true } ) );
+
+  res.render('project', {pro, logged_in : req.session.logged_in, user_id : req.session.user_id})
+
+});
+
 
 // route to get all dishes
 router.get('/profile/:id',withAuth, async (req, res) => {
